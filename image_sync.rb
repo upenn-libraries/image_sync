deead297a89f6ebfe9b799bf7166a2e49342e4c8
Katherine Lynch <katherly@upenn.edu>
2017-04-12 17:00:38 -0400

    WIP making volatile symlinks

--- image_sync.rb
+++ image_sync.rb
@@ -8,18 +8,18 @@ def missing_env_vars?
   return (ENV['IM_SOURCE'].nil? || ENV['IM_DESTINATION'].nil? || ENV['IM_VOLATILE'].nil? || ENV['IM_CANONICAL'].nil?)
 end
 
+abort 'Missing env variable(s)' if missing_env_vars?
+
+source = ENV['IM_SOURCE']
+destination = ENV['IM_DESTINATION']
+volatile = ENV['IM_VOLATILE']
+canonical = ENV['IM_CANONICAL']
+
 whitelisted_file_patterns = ['^.*DELIVERY.jp2']
 known_ignored_file_patterns = ['^.*THUMB.jp2','encoding.log']
 
 # This file will always be in the top directory of each image for processing
 identity_file = '0=dflat_1.0'
-
-abort 'Missing env variable(s)' if missing_env_vars?
-
-source = ENV['IM_SOURCE']
-dest = ENV['IM_DESTINATION']
-volatile = ENV['IM_VOLATILE']
-canonical = ENV['IM_CANONICAL']
 
 objects = Find.find(source).select { |p| /#{identity_file}/ =~ p }
 
@@ -40,17 +40,22 @@ objects.each do |object|
     basename = File.basename(file, '.*')
     basename = basename.split('.').first
 
-
     dest_basename = "#{basename}.jp2"
     dest_name_md5 = Digest::MD5.hexdigest(dest_basename)
 
     dest_dir = "#{dest_name_md5[0..2]}"
 
-    FileUtils.mkdir_p("#{dest}/#{dest_dir}")
+    volatile_file_path = "#{volatile}/#{dest_dir}/#{dest_basename}"
+    canonical_file_path = "#{canonical}/#{dest_dir}/#{dest_basename}"
+    destination_file_path = "#{destination}/#{dest_dir}/#{dest_basename}"
 
+    FileUtils.mkdir_p("#{volatile}/#{dest_dir}")
+    FileUtils.mkdir_p("#{canonical}/#{dest_dir}")
+    FileUtils.mkdir_p("#{destination}/#{dest_dir}")
 
+    FileUtils::ln_s(file, volatile_file_path)
 
-    `rsync -lptv "#{file}" "#{dest}/#{dest_dir}/#{dest_basename}"`
+    `rsync -lptv "#{file}" "#{destination_file_path}"`
 
   end
 
