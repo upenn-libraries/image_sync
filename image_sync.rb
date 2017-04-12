1b531911cb6b4942e854194a4a8f1ef0de75f51c
Katherine Lynch <katherly@upenn.edu>
2017-04-12 16:49:26 -0400

    Generalized environment variables

--- image_sync.rb
+++ image_sync.rb
@@ -4,14 +4,22 @@ require 'find'
 require 'digest/md5'
 require 'pry'
 
+def missing_env_vars?
+  return (ENV['IM_SOURCE'].nil? || ENV['IM_DESTINATION'].nil? || ENV['IM_VOLATILE'].nil? || ENV['IM_CANONICAL'].nil?)
+end
+
 whitelisted_file_patterns = ['^.*DELIVERY.jp2']
 known_ignored_file_patterns = ['^.*THUMB.jp2','encoding.log']
 
 # This file will always be in the top directory of each image for processing
 identity_file = '0=dflat_1.0'
 
-source = '/Users/kate/Documents/SharedShelf/test_area/repo/store/pairtree_root/fi/sh/er'
-dest = '/Users/kate/Documents/SharedShelf/dest'
+abort 'Missing env variable(s)' if missing_env_vars?
+
+source = ENV['IM_SOURCE']
+dest = ENV['IM_DESTINATION']
+volatile = ENV['IM_VOLATILE']
+canonical = ENV['IM_CANONICAL']
 
 objects = Find.find(source).select { |p| /#{identity_file}/ =~ p }
 
@@ -39,6 +47,8 @@ objects.each do |object|
     dest_dir = "#{dest_name_md5[0..2]}"
 
     FileUtils.mkdir_p("#{dest}/#{dest_dir}")
+
+
 
     `rsync -lptv "#{file}" "#{dest}/#{dest_dir}/#{dest_basename}"`
 
